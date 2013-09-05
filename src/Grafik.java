12d06755309fcd97102528ed262dccbac88d560c
Maximilian <senex-maximus@gmx.de>
2013-09-05 19:47:44 +0200

    mauern oben und unten

--- src/Grafik.java
+++ src/Grafik.java
@@ -5,7 +5,16 @@ import javax.swing.*;
 
 public class Grafik extends Canvas {
 	
+	int hoehe;
+	int breite;
+	LabyrinthA lab;
+	int Faktor = 1;
+	
 	public Grafik(LabyrinthA lab) {
+		
+		this.lab = lab;
+		hoehe  = lab.hoehe;
+		breite = lab.breite;
 		
 		//lab.Labyrinth[i][j].PositionX == 2;
 		setBackground(Color.pink); // weisser Hintergrund
@@ -19,11 +28,31 @@ public class Grafik extends Canvas {
 	}
 	
 	public Dimension getPreferredSize() {
-		return new Dimension(190, 190);
+		return new Dimension(this.hoehe*Faktor, this.breite*Faktor);
 	}
 
 
 	public void paint(Graphics g) {
-		g.drawLine(0,0,190,190);
+		
+		for(int i=0; i < breite; i++){
+			for(int j=0;j < hoehe; j++){
+				if (lab.Labyrinth[i][j].obererN == null  || lab.Labyrinth[i][j].obererN == Spielfeld.Rand)
+					g.drawLine(Faktor*i,Faktor*j,Faktor*i+Faktor,Faktor*j);
+				
+				if (lab.Labyrinth[i][j].untererN == null || lab.Labyrinth[i][j].untererN == Spielfeld.Rand)
+					g.drawLine(Faktor*i,Faktor*j+Faktor-1,i*Faktor+Faktor,j*Faktor+Faktor-1);
+				 
+				if (lab.Labyrinth[i][j].linkerN == null || lab.Labyrinth[i][j].linkerN == Spielfeld.Rand)
+					g.drawLine(Faktor*i,Faktor*j,i*Faktor,j*Faktor+Faktor);
+				
+				if (lab.Labyrinth[i][j].rechterN == null || lab.Labyrinth[i][j].rechterN == Spielfeld.Rand)
+					g.drawLine(Faktor*i+Faktor-1,j*Faktor,i*Faktor+Faktor-1,j*Faktor+Faktor);
+			}
+		}
 	}
 }
+
+
+
+
+
